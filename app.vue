230182489f1b6085e7fc98217a3b8c1f26a71986
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-05-22 12:27:20 +0200

    configure tailwind to match figma colors and font

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="min-h-screen w-screen flex flex-col justify-start">
-    <header class="w-100 bg-green-600 p-4 sticky top-0">
+    <header class="w-100 bg-purple p-4 sticky top-0">
       <AppHeader />
     </header>
     <main>
       <NuxtPage />
     </main>
-    <footer class="w-100 bg-green-600 p-4 mt-auto">
+    <footer class="w-100 bg-purple p-4 mt-auto">
       <AppFooter />
     </footer>
   </div>
