881945d969697348fe0c9805c9a842d949c38d75
hansmariusoveras <47254808+hansmariusoveras@users.noreply.github.com>
2024-05-22 19:54:52 +0200

    Simplify active link css

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -10,11 +10,7 @@
         <NuxtLink
           v-for="link in links"
           :key="link.path"
-          :class="
-            isActive(link.path)
-              ? 'underline underline-offset-8 decoration-2'
-              : ''
-          "
+          :active-class="'underline underline-offset-8 decoration-2'"
           :to="link.path"
         >
           {{ link.name }}
@@ -24,24 +20,11 @@
   </nav>
 </template>
 
-<script>
-export default {
-  name: 'Navbar',
-  computed: {
-    currentPath() {
-      return this.$route.path;
-    },
-    links: () => [
-      { path: '/what-we-do', name: 'What we do' },
-      { path: '/contact', name: 'Contact us' },
-      { path: '/about', name: 'About us' },
-      { path: '/staff', name: 'Our staff' },
-    ],
-  },
-  methods: {
-    isActive(path) {
-      return this.currentPath === path;
-    },
-  },
-};
+<script setup>
+const links = [
+  { path: '/what-we-do', name: 'What we do' },
+  { path: '/contact', name: 'Contact us' },
+  { path: '/about', name: 'About us' },
+  { path: '/staff', name: 'Our staff' },
+];
 </script>
