a2ce4a6c53fe02fa4366eb693e03b01500350caa
Mille Kåge <89786843+Wolframe107@users.noreply.github.com>
2024-05-24 18:49:44 +0200

    Still a little broken

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <!-- Overlay Component -->
+        <Chatbot />
+    </div>
+</template>
+
+<script setup>
+import Chatbot from '~/components/ChatBot.vue';
+</script>
+
+<style scoped>
+</style>
