003e2edc5e5330386f3f257c8055366e153ae970
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-05-29 16:52:59 +0200

    add icon to button

--- components/ButtonLink.vue
+++ components/ButtonLink.vue
@@ -10,9 +10,9 @@
 const props = defineProps({
   size: {
     type: String,
-    default: 'md',
+    default: "md",
     validator(value: string, _) {
-      return ['sm', 'md', 'lg'].includes(value);
+      return ["sm", "md", "lg"].includes(value);
     },
   },
   to: {
@@ -23,40 +23,40 @@ const props = defineProps({
 
 const padding = computed(() => {
   switch (props.size) {
-    case 'sm':
-      return 'px-6 py-2';
-    case 'md':
-      return 'px-8 py-2';
-    case 'lg':
-      return 'px-10 py-2';
+    case "sm":
+      return "px-6 py-2";
+    case "md":
+      return "px-8 py-2";
+    case "lg":
+      return "px-10 py-2";
     default:
-      return 'px-8 py-2';
+      return "px-8 py-2";
   }
 });
 
 const fontSize = computed(() => {
   switch (props.size) {
-    case 'sm':
-      return 'text-base';
-    case 'md':
-      return 'text-base';
-    case 'lg':
-      return 'text-xl';
+    case "sm":
+      return "text-base";
+    case "md":
+      return "text-base";
+    case "lg":
+      return "text-xl";
     default:
-      'text-base';
+      "text-base";
   }
 });
 
 const fontWeight = computed(() => {
   switch (props.size) {
-    case 'sm':
-      return 'font-normal';
-    case 'md':
-      return 'font-normal';
-    case 'lg':
-      return 'font-medium';
+    case "sm":
+      return "font-normal";
+    case "md":
+      return "font-normal";
+    case "lg":
+      return "font-medium";
     default:
-      'font-normal';
+      "font-normal";
   }
 });
 </script>
@@ -65,7 +65,7 @@ const fontWeight = computed(() => {
   <NuxtLink :to>
     <div
       :class="[padding, fontSize, fontWeight]"
-      class="text-white bg-purple rounded-lg text-center inline-block hover:bg-purple-900"
+      class="text-white bg-purple rounded-2xl text-center inline-block hover:bg-purple-900"
     >
       <slot></slot>
     </div>
